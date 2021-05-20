cea9cdb35fafd6d0dca806c00bfac21f1ee34b05
b729 <mowrey729@gmail.com>
2021-05-19 20:54:54 -0500

    new commit

--- models/index.js
+++ models/index.js
@@ -3,12 +3,6 @@ const Books = require('./Books');
 const Music = require('./Music');
 const Movies = require('./Movies');
 
-// Gallery.hasMany(Painting, {
-//   foreignKey: 'gallery_id',
-// });
-
-// Painting.belongsTo(Gallery, {
-//   foreignKey: 'gallery_id',
-// });
+ 
 
 module.exports = { User, Books, Movies , Music,};
